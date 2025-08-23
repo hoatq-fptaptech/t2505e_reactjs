b05dad69328490fb3bbd0f3428ffac467e60796f
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-08-23 20:43:22 +0700

    demo bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
+import { Button } from "react-bootstrap";
+
 function Home(){
     return (
         <div>
             <h1>Homepage</h1>
+            <Button variant="primary" type="button">Click me!</Button>
         </div>
     )
 };
