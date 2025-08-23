f2d895891bf45b7d6e21d7e2c02c8612d5e0dac7
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-08-23 19:40:18 +0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/Demo';
-import Demo from './components/Demo';
-import Catalog from './components/Catalog';
+import { Routes,Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+
 function App() {
-  const sv1 = {
-    name: "Nguyễn Văn An",
-    tel: "0987654321",
-    email: "annv@gmail.com",
-    address: "13 Trịnh Văn Bô"
-  };
-  const sv2 = {
-    name: "Bùi Như Lạc",
-    tel: "0123456789",
-    email: "lacbv@gmail.com",
-    address: "8A Tôn Thất Thuyết"
-  };
-  const cats = [
-    {
-      name: "Fashion",
-      count:12
-    },
-    {
-      name: "Watch",
-      count:4
-    }
-  ];
+  
   return (
     <div className="App">
-        <Demo data={sv1}/>
-        <Demo data={sv2}/>
-
-        {
-          cats.map((e,i)=>{ // e: element --- i: index
-            return <Catalog cat={e} key={i} />
-          })
-        }
+       <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+       </ul>
+       <main>
+        <div className="container">
+            <Routes>
+                <Route path="/" Component={Home} />
+                <Route path="/category" Component={Category} />
+                <Route path="/cart" Component={Cart} />
+            </Routes>
+        </div>
+       </main>
     </div>
   );
 }
--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 function Catalog(props){
     const cat = props.cat;
+    const [active,setActive] = useState("Active");
+    const [color,setColor] = useState("");
+    const disable = ()=>{ // arrow function
+        setActive("Deactive");// hiểu: active = "Deactive";
+    }
+    const favourite = ()=>{
+        if(color == "")
+            setColor("text-danger");
+        else
+            setColor("");
+    }
     return (
         <div>
             <h1>{cat.name}</h1>
             <h2>Total Item: {cat.count} products</h2>
+            <p>{active}</p>
+            <button onClick={disable} type="button">Disable</button>
+            <p><i onClick={favourite} class={"bi bi-hand-thumbs-up "+color }></i></p>
         </div>
     )
 }
